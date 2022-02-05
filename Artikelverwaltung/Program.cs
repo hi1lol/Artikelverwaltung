01edc935cc1a23fc083aed9688fff9afb2fd1393
Marcel <Marcel@MARCEL-RECHNER>
2022-02-05 01:05:51 +0100

    Projektdateien hinzufügen.

---
+++ Artikelverwaltung/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Artikelverwaltung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
